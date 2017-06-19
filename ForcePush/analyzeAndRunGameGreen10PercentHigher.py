90a7c3fcdc196e95724f9c8a8c03c2adc67a9a65
Diego Calvo <calvod@umich.edu>
2017-06-19 01:06:11 -0700

    cleaned out some comments

--- ForcePush/analyzeAndRunGameGreen10PercentHigher.py
+++ ForcePush/analyzeAndRunGameGreen10PercentHigher.py
@@ -17,23 +17,11 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pack Bonanza")
 clock = pygame.time.Clock()
 
-#diego
-#%--------------------READ IMAGE--------------------%
 
-#os.system("sudo ./usbreset /dev/bus/usb/001/" + os.popen("lsusb | grep 'C270' | grep -o 'Device....' | grep -o '...$'").read())
-#os.system("fswebcam input.jpg -r 640x360")
-#resizedInputPhoto = cv2.imread('input.jpg')
-
-
-
-
-#%--------------------RESIZE IMAGE--------------------%
-#make longest edge 400
-
-#get dimensions of image
 ticker = 200
 duplicateGreen = False
 while (True):
+    #set up screen
     screen.fill(WHITE)
     pygame.draw.line(screen, RED, [10, 10], [10+currentScore, 10], 10)
     font = pygame.font.SysFont('Calibri', 25, True, False)
@@ -42,7 +30,7 @@ while (True):
     screen.blit(text, [250, 250])
     pygame.display.flip()
     
-    
+    #take picture
     ticker = ticker + 1
     cam = cv2.VideoCapture(0)
     s, inputPhoto = cam.read()
@@ -51,32 +39,21 @@ while (True):
     #cv2.imshow("Test Picture", resizedInputPhoto)
     #cv2.waitKey(0)
     height, width, channels = inputPhoto.shape
-    print (height, width)
+    #print height, width
 
+    #resize the image so the longest edge is 300 pixels, keeping the same aspect ratio
     if height > width:
     	translationFactor = 300.0 / height
     else:
     	translationFactor = 300.0 / width
-    #print (translationFactor)
-
-    #resize the image so the longest edge is 300 pixels, keeping the same aspect ratio
+    #print translationFactor
     resizedInputPhoto = cv2.resize(inputPhoto,None,fx=translationFactor, fy=translationFactor, interpolation = cv2.INTER_CUBIC)
     #cv2.imshow('image', resizedInputPhoto)
     #cv2.waitKey(2000)
 
-		#crop the bottom 4th of the image
-		#threeFourthsDown = resizedInputPhoto.shape[0] * (3.0 / 4)
-		# print inputPhoto.shape[0]
-		# print resizedInputPhoto.shape[0]
-		# print threeFourthsDown
-		#croppedInputPhoto = resizedInputPhoto[0:threeFourthsDown, :]
-		# cv2.imshow('image', croppedInputPhoto)
-		# cv2.waitKey(0)
 
-
-		#%--------------------THRESHOLD IMAGE--------------------%
-
-		#go through each pixel
+	#%--------------------THRESHOLD IMAGE--------------------%
+	#go through each pixel
     for i in range(0, resizedInputPhoto.shape[0]):
             for j in range(0, resizedInputPhoto.shape[1]):
                     #look at the pixel
@@ -93,38 +70,21 @@ while (True):
                             green = 1
 
                     #if pixel is green enough, make it white
-                    #was 1.1
                     if red < 120 and green > 100 and blue < 130 and green > (red * 1.1) and green > (blue * 1.1):
                             resizedInputPhoto[i, j] = [255, 255, 255]
                     else: #else, make it black
                             resizedInputPhoto[i, j] = [0, 0, 0]
+	
 
-
-		#biggerYetBlurrierThresholdedImageForViewing = cv2.resize(resizedInputPhoto,None,fx=(1 / translationFactor), fy=(1 / translationFactor), interpolation = cv2.INTER_CUBIC)
-		#height, width, channels = biggerYetBlurrierThresholdedImageForViewing.shape
-		# print height, width
-		# print translationFactor
-		# print 1 / translationFactor
-		#cv2.imwrite( "CVOutput/thresholdedImage" + fn, resizedInputPhoto)
-		#cv2.imshow('image', biggerYetBlurrierThresholdedImageForViewing)
-		#cv2.waitKey(0)		
-
-
-#austin
-#for this part, use the Thresholded_Images
-#%--------------------DETECT BLOBS--------------------%
-# blobMeasurement = cv2.findContours(croppedInputPhoto, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# numberOfBlobs = len(blobMeasurement)
-# print numberOfBlobs
+    #%--------------------DETECT BLOBS--------------------%
+    # blobMeasurement = cv2.findContours(croppedInputPhoto, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # numberOfBlobs = len(blobMeasurement)
+    # print numberOfBlobs
     gray_img = cv2.cvtColor(resizedInputPhoto, cv2.COLOR_BGR2GRAY)
     #cv2.imshow('gray_img', gray_img)
     #cv2.waitKey(2000)
 
     img, contours, _ = cv2.findContours(gray_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # if fn == "stop6.jpg" or fn == "stop7.jpg" or fn == "stop8.jpg":
-    # 	cv2.imshow('img', img)
-    # 	cv2.waitKey(0)
-
     
     if len(contours) > 0:
             numberOfContours = len(contours)
@@ -133,10 +93,10 @@ while (True):
             for i in range(0, numberOfContours):
                 area = cv2.contourArea(contours[i], False)
                 # print "area of this contour is " + str(area)
-            #make sure the contour is large enough to even bother with
+                #make sure the contour is large enough to even bother with
                 if area > 500:
-                    print ("area greater than 500")
-                    print (area)
+                    #print "area greater than 500"
+                    #print area
                     #make sure the contour is square enough
                     x,y,w,h = cv2.boundingRect(contours[i])
                     # print float(w) / float(h)
@@ -150,24 +110,25 @@ while (True):
             #print "largestArea in pic "+fn+" is " + str(largestArea)
             if bestContour >= 0:
                     #cv2.drawContours(resizedInputPhoto, contours, bestContour, (0,255,0), 3)
-                    print ("found green screen in image")
+                    #print ("found green screen in image")
                     if duplicateGreen == False:
                         currentScore = currentScore + 10
                         duplicateGreen = True
                     else:
-                        print ("duplicate Green")
+                        #print ("duplicate Green")
+                        pass
                     #cv2.imwrite( "CVOutput/thresholdedWebCamImage.jpg", resizedInputPhoto)
                     #cv2.imshow('contours', resizedInputPhoto)
                     #cv2.waitKey(0)
             else:
-                    print ("found contours, but none that looked like a green screen in image: ")
+                    #print ("found contours, but none that looked like a green screen in image: ")
                     duplicateGreen = False
                     #cv2.imwrite( "CVOutput/thresholdedImage", resizedInputPhoto) 
                     #cv2.imshow('contours', resizedInputPhoto)
                     #cv2.waitKey(0)
 
     else:
-            print ("No contours found in this image: ")
+            #print ("No contours found in this image: ")
             duplicateGreen = False
 
 
